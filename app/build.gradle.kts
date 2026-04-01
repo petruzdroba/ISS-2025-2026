a1629facaa47793be70bf07632cd54b7485d8e97
Petru Zdroba <petru.zdroba@gmail.com>
2026-04-01 11:50:14 +0300

    feat: gradle settings for room and basic kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
+    id("com.android.application") version "9.0.0-alpha06" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
 }
