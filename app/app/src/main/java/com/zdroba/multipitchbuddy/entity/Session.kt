8ace1387080004d5b668c2b8259f8c5898fad27c
Petru Zdroba <petru.zdroba@gmail.com>
2026-04-03 17:09:16 +0300

    refactor: Rooms ORM foreign keys and relation mapping

--- app/app/src/main/java/com/zdroba/multipitchbuddy/entity/Session.kt
+++ app/app/src/main/java/com/zdroba/multipitchbuddy/entity/Session.kt
@@ -1,18 +1,28 @@
 package com.zdroba.multipitchbuddy.entity
 
-import android.location.Location
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.time.Instant
 
-@Entity(tableName = "sessions")
+@Entity(
+    tableName = "sessions",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("userId"),
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Session(
     @PrimaryKey(autoGenerate = true)
-    var id: Long? = null,
-    var user: User? = null,
+    var id: Long = 0,
+    val userId: Long?, // not logged in null
     var start: Instant,
     var end: Instant,
     var name: String,
-    var location: Location,
-    var events: List<ClimbEvent>
+    val latitude: Double,
+    val longitude: Double
 ) {}
--- app/app/src/main/java/com/zdroba/multipitchbuddy/entity/User.kt
+++ app/app/src/main/java/com/zdroba/multipitchbuddy/entity/User.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "users")
 data class User(
     @PrimaryKey(autoGenerate = true)
-    var id: Long? = null,
+    var id: Long = 0,
     var username: String,
     var email: String,
     var password: String
