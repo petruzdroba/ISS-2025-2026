b30d1708328b425d8499e9bab42d2ccacc6be2d2
Petru Zdroba <petru.zdroba@gmail.com>
2026-05-06 19:09:21 +0300

    init: initialization files for SpringBoot Server

---
+++ multipitch-server/src/main/kotlin/com/zdroba/multipitch_server/MultipitchServerApplication.kt
@@ -0,0 +1,11 @@
+package com.zdroba.multipitch_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MultipitchServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MultipitchServerApplication>(*args)
+}
